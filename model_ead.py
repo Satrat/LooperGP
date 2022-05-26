d7ac7a2cb9d000393967695a9c26c997835a24cf
Satrat <sara.adkins65@gmail.com>
2022-05-26 10:55:12 +0100

    load to CPU

--- model_ead.py
+++ model_ead.py
@@ -157,7 +157,8 @@ class TransformerXL(object):
         st_eopch = 0
         if pretrain_model:
             map_location = {'cuda:%d' % 0: 'cuda:%d' % self.device}
-            checkpoint = torch.load(pretrain_model, map_location=map_location)
+            print(map_location)
+            checkpoint = torch.load(pretrain_model) #, map_location=map_location)
             print('Pretrained model config:')
             print('epoch: ', checkpoint['epoch'])
             print('best_loss: ', checkpoint['best_loss'])
